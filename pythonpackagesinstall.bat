17fc5c07497744875147f39e8fc5f7665eac7a7b
jonahrichardsjar-art <jonah.richards.jar@gmail.com>
2026-05-08 14:24:01 -0400

    Add files via upload

---
+++ pythonpackagesinstall.bat
@@ -0,0 +1,10 @@
+python -m pip install --upgrade pip
+pip install matplotlib
+pip install numpy
+pip install scipy
+pip install sympy
+pip install requests
+pip install pip-review
+pip install networkx
+pip-review --local --auto
+pause
